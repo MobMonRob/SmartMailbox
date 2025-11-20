13080b208205040dcda633ef72ea669fec36d284
A1cey <mschelkle05@web.de>
2025-11-20 11:37:26 +0100

    testing camera with Python

--- client/main.py
+++ client/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from client!")
+from picamera2 import Picamera2
 
+
+def main():
+    picam2 = Picamera2()
+
+    picam2.start()
+
+    try:
+        import time
+        time.sleep(2)
+
+        picam2.capture_file("test.jpg")
+    finally:
+        picam2.stop()
 
 if __name__ == "__main__":
     main()
